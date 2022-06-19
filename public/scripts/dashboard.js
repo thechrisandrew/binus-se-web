45cacf73eab7c047a4fa62a64790efcfe660ee8d
thechrisandrew <christopherandrew612@gmail.com>
2022-06-20 01:08:17 +0700

    General code cleanup and finalization

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,18 +1,67 @@
-var data = {
-	// A labels array that can contain any sort of values
-	labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
-	// Our series array that contains series objects or in this case series data arrays
-	series: [[5, 10, 4, 2, 3]],
-};
+$(document).ready(function () {
+	function render(data) {
+		// console.log(data.todayResult[0].totalIncome);
+		$("#total-today").text(data.todayResult[0].totalIncome);
 
-var options = {
-	low: 0,
-	axisY: {
-		onlyInteger: true,
-	},
-};
+		// console.log(data.yesterdayResult[0].totalIncome);
+		$("#total-yesterday").text(data.yesterdayResult[0].totalIncome);
 
-// Create a new line chart object where as first parameter we pass in a selector
-// that is resolving to our chart container element. The Second parameter
-// is the actual data object.
-new Chartist.Line(".ct-chart", data, options);
+		var chartData = {
+			// A labels array that can contain any sort of values
+			// labels: ["Mon", "Tue", "Wed", "Thu", "Fri"],
+			labels: [],
+
+			// Our series array that contains series objects or in this case series data arrays
+			// series: [[5, 10, 4, 2, 3]],
+			series: [[]],
+		};
+
+		// console.log(data.itemStatisticResult);
+
+		data.itemStatisticResult.forEach((e) => {
+			console.log(e);
+
+			chartData.labels.push(e.transactionDate.substring(0, 10));
+
+			chartData.series[0].push(e.totalTransaction);
+		});
+
+		var options = {
+			low: 0,
+			axisY: {
+				onlyInteger: true,
+			},
+		};
+
+		// Create a new line chart object where as first parameter we pass in a selector
+		// that is resolving to our chart container element. The Second parameter
+		// is the actual data object.
+		new Chartist.Line(".ct-chart", chartData, options);
+	}
+
+	function getDashboardInfo() {
+		$.ajax({
+			type: "GET",
+			url: "http://127.0.0.1:3000/dashboard/",
+			headers: {
+				token: document.cookie
+					.split("; ")
+					.find((row) => row.startsWith("token="))
+					?.split("=")[1],
+			},
+			success: function (data, status) {
+				console.log(data);
+				// data.forEach((e, index) => {
+				// 	renderProduct(e, index);
+				// });
+				render(data);
+			},
+			error: function (data, status) {
+				console.log(data);
+				console.log(status);
+			},
+		});
+	}
+
+	getDashboardInfo();
+});
--- public/scripts/userProfile.js
+++ public/scripts/userProfile.js
@@ -1,6 +1,83 @@
-$(function(){
-    $("#signOut").click(()=>{
-	    document.cookie = "token=";
-        window.location.href = "http://127.0.0.1:5500/public/login.html"
-    });
+$(function () {
+	$("#signOut").click(() => {
+		document.cookie = "token=";
+		window.location.href = "http://127.0.0.1:5500/public/login.html";
+	});
+
+	function render(data) {
+		$("#fname-input").val(data.firstName);
+		$("#lname-input").val(data.lastName);
+		$("#email-input").val(data.email);
+	}
+
+	function getUserDetails() {
+		$.ajax({
+			type: "GET",
+			url: "http://127.0.0.1:3000/staff/user/",
+			headers: {
+				token: document.cookie
+					.split("; ")
+					.find((row) => row.startsWith("token="))
+					?.split("=")[1],
+			},
+			success: function (data, status) {
+				console.log(data);
+				render(data[0]);
+				// if (data != "Something went wrong!") {
+				// 	window.location.href = "./product.html";
+				// }
+			},
+			error: function (data, status) {
+				// console.log(data);
+				// console.log(status);
+				alert("Invalid data or Something went wrong!");
+			},
+		});
+	}
+
+	getUserDetails();
+
+	$("#edit-btn").click((event) => {
+		event.preventDefault();
+		$("#edit-btn").toggleClass("hidden");
+		$("#save-btn").toggleClass("hidden");
+
+		$("#fname-input").removeAttr("disabled");
+		$("#lname-input").removeAttr("disabled");
+		$("#email-input").removeAttr("disabled");
+	});
+
+	$("#save-btn").click((event) => {
+		event.preventDefault();
+		$("#edit-btn").toggleClass("hidden");
+		$("#save-btn").toggleClass("hidden");
+
+		$("#fname-input").attr("disabled", "disabled");
+		$("#lname-input").attr("disabled", "disabled");
+		$("#email-input").attr("disabled", "disabled");
+
+		$.ajax({
+			url: "http://127.0.0.1:3000/staff/update/",
+			type: "POST",
+			headers: {
+				token: document.cookie
+					.split("; ")
+					.find((row) => row.startsWith("token="))
+					?.split("=")[1],
+			},
+			data: {
+				email: $("#email-input").val(),
+				firstName: $("#fname-input").val(),
+				lastName: $("#lname-input").val(),
+			},
+			success: function (data, status) {
+				if (status == "success") {
+					// console.log("Save successfully");
+					alert("saved Successfully");
+				} else {
+					console.log("Not saved.");
+				}
+			},
+		});
+	});
 });
