980b8735c8682a5fb4225e0efa554fbe7514ed98
thechrisandrew <christopherandrew612@gmail.com>
2022-06-20 14:27:36 +0700

    Added min-h-screen class to body

--- public/scripts/products.js
+++ public/scripts/products.js
@@ -1,65 +1,68 @@
 $(document).ready(function () {
-    var smallTemplate = $("#smallTemplate").html();
-    var bigTemplate = $("#bigTemplate").html();
+	var smallTemplate = $("#smallTemplate").html();
+	var bigTemplate = $("#bigTemplate").html();
 
-    function renderProduct(product, index) {
-        var bigProduct = $("#bigProduct");
-        var smallProduct = $("#smallProduct");
+	function renderProduct(product, index) {
+		var bigProduct = $("#bigProduct");
+		var smallProduct = $("#smallProduct");
 
-        // console.log(product);
+		// console.log(product);
 
-        product.num = index + 1;
+		product.num = index + 1;
 
-        bigProduct.append(Mustache.render(bigTemplate, product));
-        smallProduct.append(Mustache.render(smallTemplate, product));
-    }
+		bigProduct.append(Mustache.render(bigTemplate, product));
+		smallProduct.append(Mustache.render(smallTemplate, product));
+	}
 
-    function listProduct() {
-        $.ajax({
-            type: "GET",
-            url: "http://127.0.0.1:3000/product/",
-            headers: {
-                token: document.cookie
-                    .split("; ")
-                    .find((row) => row.startsWith("token="))
-                    ?.split("=")[1],
-            },
-            success: function (data, status) {
-                // console.log(data);
-                data.forEach((e, index) => {
-                    renderProduct(e, index);
-                });
-            },
-            error: function (data, status) {
-                console.log(data);
-                console.log(status);
-            },
-        });
-    }
+	function listProduct() {
+		$.ajax({
+			type: "GET",
+			url: "http://127.0.0.1:3000/product/",
+			headers: {
+				token: document.cookie
+					.split("; ")
+					.find((row) => row.startsWith("token="))
+					?.split("=")[1],
+			},
+			success: function (data, status) {
+				// console.log(data);
+				data.forEach((e, index) => {
+					renderProduct(e, index);
+				});
+			},
+			error: function (data, status) {
+				console.log(data);
+				console.log(status);
+			},
+		});
+	}
 
-    listProduct();
+	listProduct();
 
-    $(document).on("click", ".delete-btn", null, function (event) {
-        event.preventDefault();
+	$(document).on("click", ".delete-btn", null, function (event) {
+		event.preventDefault();
 
-        var big = $(`#big${this.id}`);
-        var small = $(`#small${this.id}`);
+		var big = $(`#big${this.id}`);
+		var small = $(`#small${this.id}`);
 
-        console.log(big);
-        console.log(small);
+		console.log(big);
+		console.log(small);
 
-        big.remove();
-        small.remove();
+		big.remove();
+		small.remove();
 
-        $.ajax({
-            type: "POST",
-            url: "http://127.0.0.1:3000/product/delete/" + this.id,
-            headers: {
-                token: document.cookie
-                    .split("; ")
-                    .find((row) => row.startsWith("token="))
-                    ?.split("=")[1],
-            },
-        });
-    });
+		$.ajax({
+			type: "POST",
+			url: "http://127.0.0.1:3000/product/delete/" + this.id,
+			headers: {
+				token: document.cookie
+					.split("; ")
+					.find((row) => row.startsWith("token="))
+					?.split("=")[1],
+			},
+			success: function (data, status) {
+				alert("Successfully deleted product!");
+			},
+		});
+	});
 });
